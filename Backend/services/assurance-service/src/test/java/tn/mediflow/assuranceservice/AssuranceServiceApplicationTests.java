b06b707b31eaa938fa3b602602398f9ee9919326
Meriemelghali <meriem.elghali@esprit.tn>
2026-04-23 18:29:19 +0100

    Initial commit - Mediflow project 2

---
+++ Backend/services/assurance-service/src/test/java/tn/mediflow/assuranceservice/AssuranceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.mediflow.assuranceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssuranceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
